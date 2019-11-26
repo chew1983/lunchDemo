c5fac690a295212cc0553abab880c746014e474c
能发财能发财 <wecanbehero@163.com>
2019-11-26 19:34:12 +0800

    服务提供者-登录业务

--- menu/src/main/java/com/facai/repository/TypeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.facai.repository;
-
-import com.facai.entity.Type;
-
-import java.util.List;
-
-public interface TypeRepository {
-    public List<Type> findAll();
-}
--- order/src/main/java/com/facai/entity/Admin.java
+++
@@ -1,8 +0,0 @@
-package com.facai.entity;
-
-import lombok.Data;
-
-@Data
-public class Admin extends Account {
-
-}
